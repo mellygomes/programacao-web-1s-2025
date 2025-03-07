dc7c3f21b6a0d40fbd2bf3e8b76399321b412c7a
Emanuelly <133934109+mellygomes@users.noreply.github.com>
2025-03-07 10:00:27 -0300

    Add files via upload

---
+++ atividade01/variaveisOperadores.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 15;
+
+let soma = a + b; 
+console.log(`Resultado de {${a} + {${b}} = ${soma}}`);
+
+let sub = a - b; 
+console.log(`Resultado de {${a} - {${b}} = ${sub}}`);
+
+let mult = a * b; 
+console.log(`Resultado de {${a} * {${b}} = ${mult}}`);
+
+let div = a / b; 
+console.log(`Resultado de {${a} / {${b}} = ${div}}`);
