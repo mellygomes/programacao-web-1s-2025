74fb8b07274ab957f0370a8a8cae50982f76605e
mellygomes <emanuellygomes.manu2004@mail.com>
2025-03-28 09:51:11 -0300

    add app_calculadora

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## programacao-web-1s-2025
 ### Repositório de atividades da disciplina de programação web
 - **npm** node packed manage: pacote 
+
+- npm install express@4.21.2
--- app_calculadora/index.js
+++ app_calculadora/index.js
@@ -1,17 +1,59 @@
 const express = require('express')
-const calculo = require('./calculadora')
-
 const app = express()
 
-app.get('/', (req, resp)=>{
-    resp.send('Olá, mundo!')
+const calc = require('./calculadora')
+
+app.get('/', (req, res) => {
+    let html = '<h1>Olá, mundo!</h1>'
+    html += '<h3>Rotas disponíveis</h3>'
+    html += '<p>/somar/:a/:b (<a href="somar/2/3"> somar </a>) </p>'
+    html += '<p>/subtrair/:a/:b (<a href="subtrair/2/3"> subtrair </a>) </p>'
+    html += '<p>/multiplicar/:a/:b (<a href="multiplicar/2/3"> multiplicar </a>) </p>'
+    html += '<p>/dividir/:a/:b (<a href="dividir/2/3"> dividir </a>) </p>'
+    res.send(html)
 })
 
-app.get('/amigo:nome', (req, resp)=>{
-    resp.send(`Olá, ${req.params.nome}!`)
+app.get('/somar/:a/:b', (req, res) => {
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+
+    result = calc.sum(a, b)
+    if(isNaN(result)) {
+        res.send('Não foi possível calcular')
+    } else {
+        res.send(`${a} + ${b} = ${result}`)
+    }
+})
+
+app.get('/subtrair/:a/:b', (req, res) => {
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+
+    result = calc.sub(a, b)
+
+    res.send(`${a} - ${b} = ${result}`)
+})
+
+app.get('/multiplicar/:a/:b', (req, res) => {
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+
+    result = calc.mult(a, b)
+
+    res.send(`${a} * ${b} = ${result}`)
+})
+
+app.get('/dividir/:a/:b', (req, res) => {
+    let a = Number(req.params.a)
+    let b = Number(req.params.b)
+
+    result = calc.div(a. b)
+
+    res.send(`${a} / ${b} = ${result}`)
 })
 
 const PORT = 8080
+
 app.listen(PORT, () => {
-    console.log('app rodando na porta ' + PORT)
+    console.log('App rodando na porta ' + PORT)
 })
